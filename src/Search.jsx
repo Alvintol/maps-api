980ceed32c3475202d9347f144df14dccf0ae6cb
Alvin <alvin.tol@hotmail.com>
2022-09-30 13:54:38 -0600

    add change handler for search box

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,12 +1,33 @@
-export const Search = () => {
+import { useState } from 'react';
+
+export const Search = ({ getNewLocation }) => {
+  const [search, setSearch] = useState('');
+
+  const changeHandler = (input) => {
+    setSearch((prev) => input);
+  };
+
   return (
     <div id='search'>
-      <input 
-      id='searchBox' 
-      type='text' 
-      placeholder='Search'
+      <input
+        id='searchBox'
+        type='text'
+        placeholder='Search'
+        value={search}
+        onChange={(e) => {
+          e.preventDefault();
+          changeHandler(e.target.value);
+        }}
       />
-      <button onClick={()=> {}}>Locate</button>
+      <button
+        onClick={() => {
+          console.log('test');
+          // getNewLocation()
+          setSearch('');
+        }}
+      >
+        Locate
+      </button>
     </div>
   );
 };
