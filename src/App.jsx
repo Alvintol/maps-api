441b2f9a62a4c6c4e409812c2ef47d5b31a077ad
Alvin <alvin.tol@hotmail.com>
2022-09-30 13:25:30 -0600

    adds search box and button

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,43 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import './App.css';
 import { Map } from './Map';
+import { Search } from './Search';
 
 const App = () => {
   // Imports secret api key from .env file
   const apiKey = import.meta.env.VITE_API_KEY;
+
+  // Initializes center of map to be latitude:0 & longitude:0
   const [center, setCenter] = useState({
     lat: 0,
     lng: 0,
   });
 
+  // Loads api key to maps api in order for map to function
   const { isLoaded } = useLoadScript({ googleMapsApiKey: apiKey });
 
   useEffect(() => {
-    // Takes provided position data and returns only the required latitude and longitude
+    // Takes provided IP position data and returns only the required latitude and longitude
     const getCurrentLatLon = (position) => {
       const lat = position.coords.latitude;
       const lng = position.coords.longitude;
 
+      console.log(lat, lng);
+      // Sets map center with IP latitude/longitude
       setCenter({ lat, lng });
     };
 
+    // Activates the map to change to users city location
     navigator.geolocation.getCurrentPosition(getCurrentLatLon);
-    // console.log('GETCURRLATLON:', getCurrentLatLon)
-    console.log('LAT:', center.latitude);
-    console.log('LONG:', center.longitude);
-  }, [center]);
+  }, []);
 
   return !isLoaded ? (
     <div id='loading'>Loading Map...</div>
   ) : (
-    <Map center={center} />
+    <>
+      <Map center={center} />
+      <Search />
+    </>
   );
 };
 
---
+++ src/Search.jsx
@@ -0,0 +1,12 @@
+export const Search = () => {
+  return (
+    <div id='search'>
+      <input 
+      id='searchBox' 
+      type='text' 
+      placeholder='Search'
+      />
+      <button onClick={()=> {}}>Locate</button>
+    </div>
+  );
+};
