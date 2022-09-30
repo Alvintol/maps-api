6fb55279225b70f2e7a61cfa80c1242112e3d1f3
Alvin <alvin.tol@hotmail.com>
2022-09-30 12:41:34 -0600

    removes helper function folder

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,36 @@ import { Map } from './Map';
 const App = () => {
   // Imports secret api key from .env file
   const apiKey = import.meta.env.VITE_API_KEY;
+  const center = {
+    latitude: '',
+    longitude: '',
+  };
 
   const { isLoaded } = useLoadScript({ googleMapsApiKey: apiKey });
 
-  function getLatLon(position) {
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-      console.log("Latitude is "+latitude);
-      console.log("Longitude is "+longitude);
-  }
-  console.log('NAVIGATOR:', navigator.geolocation.getCurrentPosition(getLatLon))
+  // Takes provided position data and returns only the required latitude and longitude
+  const getCurrentLatLon = (position) => {
+    center.latitude = position.coords.latitude;
+    center.longitude = position.coords.longitude;
+
+    return {
+      latitude,
+      longitude,
+    };
+  };
+
+  // const { latitude, longitude } =
+  //   navigator.geolocation.getCurrentPosition(getCurrentLatLon);
+
+  navigator.geolocation.getCurrentPosition(getCurrentLatLon);
+  // console.log('GETCURRLATLON:', getCurrentLatLon)
+  // console.log('LAT:', latitude)
+  // console.log('LONG:', longitude)
 
   return !isLoaded ? (
     <div id='loading'>Loading Map...</div>
   ) : (
-    <Map 
-    center={center}
-    />
+    <Map center={center} />
   );
 };
 
