8b174501bf506a0678eeb9acdbec90b0b72a1722
Alvin <alvin.tol@hotmail.com>
2022-09-30 11:35:36 -0600

    adds Map component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { GoogleMap, useLoadScript } from '@react-google-maps/api';
 import './App.css';
 
 const App = () => {
-
   // Imports secret api key from .env file
   const apiKey = import.meta.env.VITE_API_KEY;
 
-  const {} = useLoadScript({'googleMapsApiKey': apiKey});
-  return <div className='app'>Map</div>;
+  const { isLoaded } = useLoadScript({ googleMapsApiKey: apiKey });
+  
+  !isLoaded ? 
+    <div id='loading'>Loading Map...</div> : 
+    <div id='app'>Map</div>;
 };
 
 export default App;
