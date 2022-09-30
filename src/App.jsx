ee6e9a1e3acc690f9ce9794035688da1bf8669f3
Alvin <alvin.tol@hotmail.com>
2022-09-30 12:48:17 -0600

    initialized state as center object

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useLoadScript } from '@react-google-maps/api';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
 import { Map } from './Map';
 
 const App = () => {
   // Imports secret api key from .env file
   const apiKey = import.meta.env.VITE_API_KEY;
-  const center = {
-    latitude: '',
-    longitude: '',
-  };
+  const [center, setCenter] = useState({
+    lat: 0,
+    lng: 0,
+  });
 
   const { isLoaded } = useLoadScript({ googleMapsApiKey: apiKey });
 
@@ -16,20 +18,14 @@ const App = () => {
   const getCurrentLatLon = (position) => {
     center.latitude = position.coords.latitude;
     center.longitude = position.coords.longitude;
-
-    return {
-      latitude,
-      longitude,
-    };
+    console.log(typeof center.latitude);
+    console.log(typeof center.longitude);
   };
-
-  // const { latitude, longitude } =
-  //   navigator.geolocation.getCurrentPosition(getCurrentLatLon);
 
   navigator.geolocation.getCurrentPosition(getCurrentLatLon);
   // console.log('GETCURRLATLON:', getCurrentLatLon)
-  // console.log('LAT:', latitude)
-  // console.log('LONG:', longitude)
+  console.log('LAT:', center.latitude);
+  console.log('LONG:', center.longitude);
 
   return !isLoaded ? (
     <div id='loading'>Loading Map...</div>
